628f7962690f2bddba9c3e35a995b203d1d8795c
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-09-06 23:08:59 +0100

    😉 rendering views

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   console.log("Here");
-  res.download("./server.js");
+  res.render("index", { text: "World!" });
 });
 
 app.listen(3000, () => {
