c70071d5f2ce9b78b8b7e0f388ffb12d1830ce06
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-09-06 23:38:48 +0100

    😁 using logger middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(logger); // logger is defined at the top
 
 app.get("/", (req, res) => {
   console.log("Here");
@@ -11,6 +12,12 @@ app.get("/", (req, res) => {
 const usersRouter = require("./routes/users");
 
 app.use("/users", usersRouter);
+
+// middleware function
+function logger(req, res, next) {
+  console.log(req.method + " " + req.originalUrl);
+  next();
+}
 
 app.listen(3000, () => {
   console.log("server running at http://localhost:3000");
